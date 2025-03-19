a8739fb781af231afaaa95296cb1551ad52db5b1
pjehan <pierre.jehan@gmail.com>
2025-03-19 12:44:28 +0100

    Installation de Mongoose et connexion à la base de données via Mongoose

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const { db } = req.app.locals;
+  await db.collection('projects').insertOne({ name: 'test' });
+  const projects = await db.collection('projects').find().toArray();
+  res.json(projects);
 });
 
 export default router;
